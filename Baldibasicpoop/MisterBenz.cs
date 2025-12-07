9f5444b7fd1cb6416e2a29052dc2cd510b2135a8
heptatonic-cpu <heptatonicscaling@gmail.com>
2025-12-07 20:39:02 +0800

    Update MisterBenz.cs

--- Baldibasicpoop/MisterBenz.cs
+++ Baldibasicpoop/MisterBenz.cs
@@ -14,13 +14,46 @@ namespace Baldibasicpoop
             base.Navigator.SetSpeed(wanderSpeed);
             base.Navigator.maxSpeed = wanderSpeed;
             spriteRenderer[0].sprite = BasePlugin.Instance.assetMan.Get<Sprite>("Benz_Idle"); // collects the sprite inside the assetmanager on the plugin
+            pam = base.GetComponent<PropagatedAudioManager>();
+		
         }
 
-        public void Explode() // just added this cuz your npc will explode for s
+        public void Explode(Collider other) // just added this cuz your npc will explode for s
         {
+            if (!collided)
+            {
+                other.GetComponent<Entity>().AddForce(new Force(Vector3.zero, 10f, -10f));
+                collided = true;
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (collided)
+            {
+                if (timeToAppear > 0f)
+                {
+                    timeToAppear -= Time.deltaTime;
+                }
+				Navigator.Entity.SetInteractionState(false);
+				spriteRenderer[0].gameObject.SetActive(false);
+            }
+            if (timeToAppear <= 0f)
+            {
+                collided = false;
+                Navigator.Entity.SetInteractionState(true);
+				spriteRenderer[0].gameObject.SetActive(true);
+            }
+            
         }
 
         public float wanderSpeed = 10f;
         public float chaseSpeed = 18f; // i added this if your npc chases the player
+
+        public bool collided;
+        public float timeToAppear = 15f;
+
+        [SerializeField]
+        public PropagatedAudioManager pam;
     }
 }
